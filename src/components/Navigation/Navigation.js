95d0970fba84ccede688c63dead672c4bc7b4961
leonyangela <leonyangela@DESKTOP-1KNN1EJ>
2020-05-27 14:20:18 +0700

    Change some code, Connect Frontend to Backend

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Navigation = ({onRouteChange, isSignedIn}) => {
-    if(isSignedIn) {
+    if (isSignedIn) {
         return (
             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
                 <p onClick={() => onRouteChange('signout')} 
